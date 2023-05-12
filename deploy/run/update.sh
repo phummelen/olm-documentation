76bc38a042b5124ff770a6e0ff174c0e8eb465e4
Pieter Jan Hummelen <phummelen@maxedy.com>
2023-05-12 22:12:06 +0200

    changed github action

--- deploy/run/update.sh
+++ deploy/run/update.sh
@@ -3,9 +3,9 @@ SETENV_FILE=${SETENV_FILE:-./setenv.sh}
 . ${SETENV_FILE}
 . ./defaults.sh
 
-set +e
-olm-docs-imageid=$(docker inspect --format='{{.Image}}' olm-${ENVIRONMENT})
-set -e
+#set +e
+#olm-docs-imageid=$(docker inspect --format='{{.Image}}' ${ENVIRONMENT}-olm-docs)
+#set -e
 
 # pull the latest container
 docker-compose ${COMPOSE_FILES} -p olm-${ENVIRONMENT} pull
@@ -14,13 +14,13 @@ docker-compose ${COMPOSE_FILES} -p olm-${ENVIRONMENT} pull
 ./stop.sh
 
 # remove the current container instance (removes copy-on-write-layer too)
-(docker rm -v olm-${ENVIRONMENT} || true)
+(docker rm -v ${ENVIRONMENT}-olm-docs || true)
 
 # remove the old image (if it is not used anymore)
-if [ "$(docker inspect --format='{{.Image}}' olm-${ENVIRONMENT})" != "${olm-docs-imageid}" ]
-then
-	(docker rmi ${olm-docs-imageid} || true)
-fi
+#if [ "$(docker inspect --format='{{.Image}}' ${ENVIRONMENT}-olm-docs)" != "${olm-docs-imageid}" ]
+#then
+#	(docker rmi ${olm-docs-imageid} || true)
+#fi
 
 # start the container
 ./start.sh
